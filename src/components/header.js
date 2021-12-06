b65428143c6f6582ffdfd6779487ff0a3aeef9d8
writinginlee <95411978+writinginlee@users.noreply.github.com>
2021-12-05 20:40:43 -0600

    Add tablet/desktop header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
 
 import * as styles from "./header.module.css";
@@ -39,21 +39,56 @@ export const Header = () => {
     [simplifiedPosts]
   );
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const onToggleHeader = () => {
+    if (dropdownOpen) {
+      setDropdownOpen(false);
+    } else {
+      setDropdownOpen(true);
+    }
+  };
+
   return (
-    <header>
-      <nav>
-        <Link to={"/"} key={"/"} className={styles.link}>
-          Home
-        </Link>
-        <Link to={"/about"} key={"/about"} className={styles.link}>
-          About
-        </Link>
-        <Link to={"/blog"} key={"/blog"} className={styles.link}>
-          Blog
-        </Link>
-        <Link to={"/projects"} key={"/projects"} className={styles.link}>
-          Projects
-        </Link>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.flex}>
+          <Link to={"/"} key={"/"} className={styles.link}>
+            Home
+          </Link>
+        </div>
+        <div className={styles.flex}>
+          <Link to={"/about"} key={"/about"} className={styles.link}>
+            About
+          </Link>
+          <Link to={"/blog"} key={"/blog"} className={styles.link}>
+            Blog
+          </Link>
+          <button
+            className={styles.button}
+            onClick={onToggleHeader}
+            onBlur={onToggleHeader}
+          >
+            <span>Categories ▾</span>
+            <ul className={dropdownOpen ? styles.submenuOpen : styles.submenu}>
+              {categories.map((category) => (
+                <li>
+                  {" "}
+                  <Link
+                    to={`/categories/${category}`}
+                    key={category}
+                    className={styles.subLink}
+                  >
+                    {category}
+                  </Link>{" "}
+                </li>
+              ))}
+            </ul>
+          </button>
+          <Link to={"/projects"} key={"/projects"} className={styles.link}>
+            Projects
+          </Link>
+        </div>
       </nav>
     </header>
   );
