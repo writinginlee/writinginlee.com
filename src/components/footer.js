64b8adf8411d34421adc2eed419177c6786c5ae6
writinginlee <95411978+writinginlee@users.noreply.github.com>
2021-12-07 01:14:06 -0600

    Add container to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,26 +8,28 @@ import * as styles from "./footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.flex}>
-        <nav className={styles.nav}>
-          <span className={styles.span}>© Portfolio 2021</span>
-          <Link to={"/rss.xml"} key={"/rss.xml"} className={styles.link}>
-            RSS
-          </Link>
-        </nav>
-        <nav className={styles.nav}>
-          <a
-            href={"https://github.com/writinginlee"}
-            title={"GitHub"}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={"https://github.com/writinginlee"}
-            className={styles.link}
-          >
-            <span className={styles.span}>GitHub</span>
-            <img src={github} alt="GitHub" width="20" height="20" />
-          </a>
-        </nav>
+      <div className={styles.container}>
+        <div className={styles.flex}>
+          <nav className={styles.nav}>
+            <span className={styles.span}>© Portfolio 2021</span>
+            <Link to={"/rss.xml"} key={"/rss.xml"} className={styles.link}>
+              RSS
+            </Link>
+          </nav>
+          <nav className={styles.nav}>
+            <a
+              href={"https://github.com/writinginlee"}
+              title={"GitHub"}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={"https://github.com/writinginlee"}
+              className={styles.link}
+            >
+              <span className={styles.span}>GitHub</span>
+              <img src={github} alt="GitHub" width="20" height="20" />
+            </a>
+          </nav>
+        </div>
       </div>
     </footer>
   );
