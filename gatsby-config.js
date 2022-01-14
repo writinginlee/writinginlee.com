8a6f8f625a32ee28656e859b8cfc2065b8a434f3
writinginlee <95411978+writinginlee@users.noreply.github.com>
2022-01-14 00:46:48 -0500

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,6 +114,7 @@ module.exports = {
           "gatsby-remark-autolink-headers",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
+          "gatsby-remark-emoji",
           {
             resolve: "gatsby-remark-images",
             options: {
